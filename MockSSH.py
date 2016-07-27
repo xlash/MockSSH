74f3d8268b39137e7934365ea0947c8b4c3d8cc1
Guillaume Nourry-Marquis <guillaume.nourry.marquis@gmail.com>
2016-07-27 10:40:08 -0400

    Adjusting default stdout handler for logger

--- MockSSH.py
+++ MockSSH.py
@@ -39,8 +39,9 @@ __all__ = (
     "stopThreadedServer"
 )
 
-logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
 
 class SSHServerError(Exception):
     """Raised when an SSH server error is encountered"""
